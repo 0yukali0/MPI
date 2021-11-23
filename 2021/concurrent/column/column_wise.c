458c7229dff692f2a0f37b7e6b8221736ca709cf
YuTeng Chen <45888688+0yukali0@users.noreply.github.com>
2021-11-23 23:57:01 +0800

    Fix time record timing

--- 2021/concurrent/column/column_wise.c
+++ 2021/concurrent/column/column_wise.c
@@ -54,7 +54,6 @@ int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &pid);
   MPI_Comm_size(MPI_COMM_WORLD, &group);
-  seqStartTime = MPI_Wtime();
   // defualt role is slave
   role.init = SlaveInit;
   role.work = Work;
